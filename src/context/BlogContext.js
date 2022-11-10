ecc0b5a6b4aa83bf380b38b1d8d040604ef20d78
Swag Finger <swagfinger@gmail.com>
2022-11-10 11:47:41 +0800

    COMPLETED: useReducer to manage functions

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext = React.createContext();
 
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `blog post #${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
 
-  //todo: change functions to useReducer()
+export const BlogProvider = ({ children }) => {
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
   const addBlogPost = () => {
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog Post #${blogPosts.length + 1}` }
-    ]);
+    dispatch({ type: 'add_blogpost' });
   };
 
   return (
